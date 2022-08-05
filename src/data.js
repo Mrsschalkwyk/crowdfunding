c9ead597a41d0df28ca4685c16c1e3130c546127
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-08-05 21:02:56 +1000

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const allProjects = [
+    {
+        id:1,
+        title: "Project One",
+        description:"First Project",
+        goal: 150,
+        image: "https://via.placeholder.com/300.jpg",
+        is_open:  false,
+        data_created: "2020-03-20T14:22:23.382748Z",
+        owner:1,
+    },
+
+]
+
+export const oneProject ={
+    id:1,
+    title: "Project one",
+    description: "First p=project",
+    goal: 150,
+    image: "https://via.placeholder.com/300.jpg",
+    is_open:false,
+    date_created: "2020-03-20T14:22:23.382748Z",
+    owner:1,
+    pledges: [
+        {
+            id :1,
+            amount:100,
+            comment: " comment for pledge",
+            anonymouse: false,
+            supporter: 3,
+            project_id:1,
+        },
+    ],
+
+};
